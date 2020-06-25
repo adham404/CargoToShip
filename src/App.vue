ec655473b00aae0eb56002cd4a7f0cf6ae846733
adham404 <adham_mohsen@outlook.com>
2020-06-25 21:56:08 +0200

    Third commit fixed the App.vue problem

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
-
 <template>
   <div id="app">
-
+    <h1>hello</h1>
   </div>
 </template>
-<script>
-      export default {
 
-      };
+
+<script>
 </script>
 
 
--- src/router/index.js
+++ src/router/index.js
@@ -8,21 +8,21 @@ const routes = [
   // {
   //   path: '/',
   //   name: 'Map',
-  //   component: () => import('@/components/Map.vue')
+  //   component: () => import('@/App.vue')
   // },
   // {
   //   path: '/home',
   //   name: 'Home',
   //   component: Home,
   // },
-  {
+  // {
     // path: '/about',
     // name: '',
     // // route level code-splitting
     // // this generates a separate chunk (about.[hash].js) for this route
     // // which is lazy-loaded when the route is visited.
     // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  // }
 ]
 
 const router = new VueRouter({
