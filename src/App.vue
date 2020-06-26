7e4f059360adb1cb8be096adafc1f3e57d44e124
adham404 <adham_mohsen@outlook.com>
2020-06-26 21:11:52 +0200

    I added the firebase connection and setup the firebase rest api along with the realtime database

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,37 @@
 <template>
   <div id="app">
     <h1>hello</h1>
+    <input type="text" name="" value="" v-model = "input">
+    <input type="submit" value="Submit" @click="sendnow()" />
+    <p>{{input}}</p>
+    <div id="map">
+
+    </div>
   </div>
+
 </template>
 
 
 <script>
+export default {
+data() {
+  return {
+    input: "",
+  };
+},
+methods: {
+  sendnow() {
+    /*eslint-disable*/
+    firebase.database().ref('lists/' + this.input ).set({
+      name: this.input,
+    });
+    alert(this.input)
+}
+},
+mounted(){
+  alert(simplemaps_worldmap.mapdata.main_settings.current_id)
+}
+};
 </script>
 
 
--- src/main.js
+++ src/main.js
@@ -1,12 +1,31 @@
+/*eslint-disable*/
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import "./plugins/axios";
+import firebase from 'firebase';
+// import simplemaps_worldmap from  "../public/worldmap"
 
 
 
+
+//This is the configuration for firebase I pasted the config object and then inserted it into the initializeApp function
+var firebaseConfig = {
+  apiKey: "AIzaSyBSMAICYcm8WxTIlEPTCmRLuoR6s4vwiLM",
+  authDomain: "cargo2ship-c9662.firebaseapp.com",
+  databaseURL: "https://cargo2ship-c9662.firebaseio.com",
+  projectId: "cargo2ship-c9662",
+  storageBucket: "cargo2ship-c9662.appspot.com",
+  messagingSenderId: "1062829855943",
+  appId: "1:1062829855943:web:809c71cde7d3af29ccdae2",
+  measurementId: "G-W62MFYZRES"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
 Vue.config.productionTip = false;
 Vue.prototype.$senior_seen = false;
+
 
 
 
