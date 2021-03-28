140e6407b90f3fa0e47cabfe75d8399d3db85a44
adham404 <adham_mohsen@outlook.com>
2021-03-29 00:15:32 +0200

    Added the map now I have to configure and style it and add the logic and possibly other things I think

---
+++ src/components/HomePage/Map.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>hello</h1>
+        <div id="map"></div>
+    </div>
+</template>
+
+<script>
+/*eslint-disable*/
+
+    export default {
+        mounted(){
+            simplemaps_worldmap.load();
+        },
+        computed: {
+            simplemaps_worldmap: function () {return window.simplemaps_worldmap;}
+        }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -18,7 +18,17 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/Map",
+    name: "Map",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "@/components/HomePage/Map.vue")
   }
+
 ];
 
 const router = new VueRouter({
